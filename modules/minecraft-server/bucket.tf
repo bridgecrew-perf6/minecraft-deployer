bea267d57252ff439912042d6c19c00f77af2cf5
Mark Cooper <mark.c.cooper@outlook.com>
2022-04-10 11:56:04 -0700

    Restrict bucket more

--- modules/minecraft-server/bucket.tf
+++ modules/minecraft-server/bucket.tf
@@ -18,8 +18,10 @@ resource "aws_s3_bucket_acl" "minecraft" {
 resource "aws_s3_bucket_public_access_block" "minecraft" {
   bucket = aws_s3_bucket.minecraft.id
 
-  block_public_acls   = true
-  block_public_policy = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_versioning" "minecraft" {
